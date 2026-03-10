35ef2edb4b9c97e24dc43e9597b80899de687326
Debesh Dinda <debeshdinda993@gmail.com>
2026-03-11 00:22:59 +0530

    url changed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://student-crud-phi-gules.vercel.app/', // your frontend URL
+    origin: '*', // your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
